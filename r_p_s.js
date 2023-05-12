0c8ef3a0b3502dff640e5c5e8698aa780bc02fd6
Aaron Shippey <aaronshippey.bus@gmail.com>
2023-05-12 18:11:51 -0400

    Creat javascript file

---
+++ r_p_s.js
@@ -0,0 +1,43 @@
+const choice = ['Rock','Paper','Scissors']
+let userSelection = 'Rock'
+let computerSelection
+
+
+const outcomeMessage = ['Congrats, you win!', 'Sorry, you lose.','Nobody wins. It\'s a draw']
+
+if (userselection == 'Rock' && computerSelection == 'Scissors'){outcomeMessage[1]}
+
+else if (userSelection == 'Rock' && omputerSelection == 'Rock') {
+    outcomeMessage[3]
+}
+
+else if (userSelection == 'Rock' && computerSelection == 'Paper') {
+    outcomeMessage[2]
+}
+
+else if (userSelection = 'Scissors' && computerSelection == 'Scissors') {
+    outcomeMessage[3]
+}
+
+else if (userSelection = 'Scissors' && computerSelection == 'Rock') {
+    outcomeMessage[2]
+}
+
+else if (userSelection = 'Scissors' && computerSelection == 'Paper') {
+    outcomeMessage[1]
+}
+
+else if (userSelection = 'Paper' && computerSelection == 'Paper') {
+    outcomeMessage[3]
+}
+
+else if (userSelection = 'Paper' && computerSelection == 'Rock') {
+    outcomeMessage[1]
+}
+
+else if (userSelection = 'Paper' && computerSelection == 'Scissors') {
+    outcomeMessage[2]
+}
+
+function getComputerSelection (answer) {
+}
