036c9104e8e2f0a32f4b763e8e58df955f7c968a
Aaron Shippey <aaronshippey.bus@gmail.com>
2023-05-17 17:43:02 -0400

    add return statement to getComputerSelection function

--- r_p_s.js
+++ r_p_s.js
@@ -1,43 +1,57 @@
-const choice = ['Rock','Paper','Scissors']
-let userSelection = 'Rock'
-let computerSelection
+const playerSelection = window.prompt("What is your selection")
 
+
+const choice = [
+  "Rock",
+  "Paper",
+  "Scissors"
+];
 
 const outcomeMessage = ['Congrats, you win!', 'Sorry, you lose.','Nobody wins. It\'s a draw']
 
-if (userselection == 'Rock' && computerSelection == 'Scissors'){outcomeMessage[1]}
+const computerSelection = getComputerSelection ()
 
-else if (userSelection == 'Rock' && omputerSelection == 'Rock') {
+function getComputerSelection (randomString) {if (playerSelection = 'Rock' || 'Paper' || 'Scissors')} {const computerSelection = choice[Math.floor(Math.random() * choice.length)];
+return computerSelection;}
+
+
+function playRound (playerSelection, computerSelection)
+{
+
+    if (playerSelection == 'Rock' && computerSelection == 'Scissors'){outcomeMessage[1]}
+
+else if (playerSelection == 'Rock' && computerSelection == 'Rock') {
     outcomeMessage[3]
 }
 
-else if (userSelection == 'Rock' && computerSelection == 'Paper') {
+else if (playerSelection == 'Rock' && computerSelection == 'Paper') {
     outcomeMessage[2]
 }
 
-else if (userSelection = 'Scissors' && computerSelection == 'Scissors') {
+else if (playerSelection == 'Scissors' && computerSelection == 'Scissors') {
     outcomeMessage[3]
 }
 
-else if (userSelection = 'Scissors' && computerSelection == 'Rock') {
+else if (playerSelection == 'Scissors' && computerSelection == 'Rock') {
     outcomeMessage[2]
 }
 
-else if (userSelection = 'Scissors' && computerSelection == 'Paper') {
+else if (playerSelection == 'Scissors' && computerSelection == 'Paper') {
     outcomeMessage[1]
 }
 
-else if (userSelection = 'Paper' && computerSelection == 'Paper') {
+else if (playerSelection == 'Paper' && computerSelection == 'Paper') {
     outcomeMessage[3]
 }
 
-else if (userSelection = 'Paper' && computerSelection == 'Rock') {
+else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
     outcomeMessage[1]
 }
 
-else if (userSelection = 'Paper' && computerSelection == 'Scissors') {
+else if (playerSelection == 'Paper' && computerSelection == 'Scissors') {
     outcomeMessage[2]
 }
 
-function getComputerSelection (answer) {
 }
+
+playRound (playerSelection, computerSelection)
