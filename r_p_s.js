53154532ff8029001461c4c21326d702e2a72134
Aaron Shippey <aaronshippey.bus@gmail.com>
2023-09-10 08:29:04 -0400

    add event listners

--- r_p_s.js
+++ r_p_s.js
@@ -1,48 +1,10 @@
-function playRound (playerSelection, computerSelection)
+const buttons = document.querySelectorAll('button');
 
-{if (playerSelection == 'Rock' && computerSelection == 'Scissors'){'Congrats, you win. Rock beats scissors'}
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
 
-else if (playerSelection == 'Rock' && computerSelection == 'Paper') {
-   alert ('Sorry, you lose. paper beats rock')
-}
-
-else if (playerSelection == 'Scissors' && computerSelection == 'Rock') {
-   alert ('Sorry, you lose. Rock beats scissors.')
-}
-
-else if (playerSelection == 'Scissors' && computerSelection == 'Paper') { alert ('Congrats, you win. Scissors beats Rock.')
-}
-
-else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
-   alert ('Congrats, you win. paper beats rock.')
-}
-
-else if (playerSelection == 'Paper' && computerSelection == 'Scissors') {
- alert ('Sorry, you lose. Scissors beats paper.')
-}
-
-else if (playerSelection == computerSelection) {
- alert ("It's a draw, try again.")
-}
-
-return playerSelection, computerSelection;
-
-}
-const playerSelection = window.prompt("What is your selection")
-
-function getComputerSelection (randomString){const computerSelection = choice[Math.floor(Math.random() * choice.length)];
-return computerSelection;}
-
-function game ()
-
-
-const choice = [
-  "Rock",
-  "Paper",
-  "Scissors"
-];
-
-const computerSelection = getComputerSelection ()
-
-if (playerSelection ==  "Rock"||"Paper"||"Scissors" && computerSelection == "Rock"||"Paper"||"Scissors" ) 
-{playRound (playerSelection, computerSelection)}
+  // and for each one we add a 'click' listener
+  button.addEventListener('click', () => {
+    document.body.style.backgroundColor: "blue";
+  });
+});
